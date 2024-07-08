d6b11d21a66f36aced6ab291b0a0e5a1e0247f98
Nitin Verma <36342305+nitinverma78@users.noreply.github.com>
2024-07-08 16:28:18 +0000

    add nbdev

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ source ~/.venv/bin/activate
 #append it to bash so every shell launches with it 
 echo 'source ~/.venv/bin/activate' >> ~/.bashrc
 #Below only used if conda is needed)
-#make install-tensorflow-conda 
+#make install-tensorflow-conda
+pip install -r ./requirements.txt 
